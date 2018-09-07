8b1efbaa16e7aac9d0f216ede95f29f7980eb09a
duqu <duqu@eastmoney.com>
2018-09-07 20:10:24 +0800

    UP

---
+++ precache-manifest.df58374f7e4f2a710cb07230686cbb26.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8c7728c457f557e1c035",
+    "url": "/js/index.714a05be.js"
+  },
+  {
+    "revision": "7fd478d486876b604402",
+    "url": "/js/e6da.7c8e791a.js"
+  },
+  {
+    "revision": "13c0626f6c70a814a4f7",
+    "url": "/js/control.2d2f3044.js"
+  },
+  {
+    "revision": "5c4212c35488b93af87f",
+    "url": "/js/chunk-vendors.2d0f8144.js"
+  },
+  {
+    "revision": "75409ac7f84a3042713d428a19efbbb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c7728c457f557e1c035",
+    "url": "/css/index.38825899.css"
+  },
+  {
+    "revision": "13c0626f6c70a814a4f7",
+    "url": "/css/control.de1fa0fe.css"
+  },
+  {
+    "revision": "13bd268f592d2db19e46d6833a997f10",
+    "url": "/control.html"
+  }
+];
